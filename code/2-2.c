a4ce51f2231a47ce5aefbc3debc0e8a59c4a7a52
thototh <144813512+thototh@users.noreply.github.com>
2026-04-24 11:27:29 +0900

    Update 2-2.c

---
+++ code/2-2.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <string.h>
+
+void foo(){
+    char *str1 = "hello";
+    printf("%p\n", (void*)str1);
+}
+
+void bar(){
+    char *str2 = "hello";
+    printf("%p\n", (void*)str2);
+}
+
+int main() {
+    
+    foo();
+    bar();
+
+    return 0;
+}
